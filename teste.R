6330392fbc5ef6af5d68521b8bab16bfd77de89a
Maurício Lordêlo <mslordelo@gmail.com>
2022-02-20 18:24:18 -0300

    Add files via upload
    Add here by navigator

---
+++ teste.R
@@ -0,0 +1,11 @@
+#usethis::use_readme_md()
+x = 4
+y = 5
+x + y
+
+credentials::set_github_pat("MyFirst")
+
+
+#usethis:: git clone https://github.com/username/repo.git
+#Username: MauricioLordelo
+#Password: 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
