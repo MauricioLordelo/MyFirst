e29b01553b8614a9bd51eb8a567efb77651278de
Maurício Lordêlo <mslordelo@gmail.com>
2022-02-20 18:22:02 -0300

    Token inserido

--- teste.R
+++ teste.R
@@ -2,3 +2,10 @@
 x = 4
 y = 5
 x + y
+
+credentials::set_github_pat("MyFirst")
+
+
+#usethis:: git clone https://github.com/username/repo.git
+#Username: MauricioLordelo
+#Password: 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
